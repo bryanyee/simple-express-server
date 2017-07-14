a934c83e6f223b3a00908cddad82ab61925989ee
Bryan Yee <bryan.yee@grandrounds.com>
2017-07-14 00:08:43 -0700

    Add json endpoint

--- server/server.js
+++ server/server.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to port 3000!');
 })
+
+app.get('/json', (req, res) => {
+  const peoplePath = path.join('server', 'data', 'people.json');
+  fs.readFile(peoplePath, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+    }
+    const jsonData = data.toString();
+    res.set('Content-Type', 'application/json')
+      .status(200)
+      .send(jsonData);
+  });
+});
 
 app.get('/timeout', (req, res) => {
   res.status(408).send('Request timed out.');
